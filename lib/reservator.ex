00c447bc3527779a0c1cd7d0f392915215c65754
zastrixarundell <luka@bodroza.com>
2024-10-02 22:34:46 +0200

    Add documentation within the main module

--- lib/reservator.ex
+++ lib/reservator.ex
@@ -1,5 +1,19 @@
 defmodule Reservator do
   @moduledoc """
-  Documentation for `Reservator`.
+  A technical, test, elixir application.
+
+  This is an app for the showcase of technical experience.
+
+  This is primarily meant to used directly in the CLI.
+
+  To start the application run:
+
+      mix reservator
+    
+  In case a custom input file is required, run:application
+
+      mix reservator custom_input.txt
+    
+  Where the input path can be either relative or absolute.
   """
 end
