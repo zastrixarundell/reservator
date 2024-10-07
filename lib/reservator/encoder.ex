0e8bee88af9653f31359d5ed26d65bd875ac62a3
zastrixarundell <luka@bodroza.com>
2024-10-07 19:37:03 +0200

    Drop edge-case support

--- lib/reservator/encoder.ex
+++ lib/reservator/encoder.ex
@@ -32,46 +32,6 @@ defmodule Reservator.Encoder do
       ...>   ]
       ...> )
       "TRIP to NYC\\nFlight from SVQ to BCN at 2023-03-02 06:40 to 09:10\\nFlight from BCN to NYC at 2023-03-02 15:00 to 22:45"
-      
-      iex> Reservator.Encoder.convert_to_string(
-      ...>   "SVQ",
-      ...>   [
-      ...>     [
-      ...>       %Reservator.Reservation.Segment{
-      ...>         segment_type: "Flight",
-      ...>         start_time: ~N[2023-03-02 06:40:00],
-      ...>         start_location: "SVQ",
-      ...>         end_time: ~N[2023-03-02 09:10:00],
-      ...>         end_location: "BCN"
-      ...>       },
-      ...>       %Reservator.Reservation.Segment{
-      ...>         segment_type: "Flight",
-      ...>         start_time: ~N[2023-03-02 15:00:00],
-      ...>         start_location: "BCN",
-      ...>         end_time: ~N[2023-03-02 22:45:00],
-      ...>         end_location: "NYC"
-      ...>       },
-      ...>       %Reservator.Reservation.Segment{
-      ...>         segment_type: "Flight",
-      ...>         start_time: ~N[2023-03-02 15:00:00],
-      ...>         start_location: "NYC",
-      ...>         end_time: ~N[2023-03-03 00:00:00],
-      ...>         end_location: "BCN"
-      ...>       },
-      ...>       %Reservator.Reservation.Segment{
-      ...>         segment_type: "Flight",
-      ...>         start_time: ~N[2023-03-03 15:00:00],
-      ...>         start_location: "BCN",
-      ...>         end_time: ~N[2023-03-03 22:45:00],
-      ...>         end_location: "NYC"
-      ...>       }
-      ...>     ]
-      ...>   ]
-      ...> )
-      "TRIP to NYC, BCN, NYC\\nFlight from SVQ to BCN at 2023-03-02 06:40 to 09:10\\n" <>
-        "Flight from BCN to NYC at 2023-03-02 15:00 to 22:45\\n" <>
-        "Flight from NYC to BCN at 2023-03-02 15:00 to 00:00\\n" <>
-        "Flight from BCN to NYC at 2023-03-03 15:00 to 22:45"
   """
   @spec convert_to_string(starting_location :: String.t(), segments :: list(list(Segment.t()))) ::
           String.t()
