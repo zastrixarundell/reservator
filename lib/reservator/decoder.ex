b2e2bf8adbbff5ee844d945a668bd95a24db019a
zastrixarundell <luka@bodroza.com>
2024-10-07 23:29:34 +0200

    Update decoder

--- lib/reservator/decoder.ex
+++ lib/reservator/decoder.ex
@@ -7,12 +7,53 @@ defmodule Reservator.Decoder do
 
   require Logger
 
+  @location_regex ~r/^BASED: (?<base>[A-Z]{3})$/m
+
   @doc """
   Decodes the input file into usable information.
 
   ## Examples
 
-      iex> Reservator.Decoder.decode_file("input.txt")
+      iex> Reservator.Decoder.read_file("input.txt")
+      {
+        :ok,
+        \"\"\"
+        BASED: SVQ
+
+        RESERVATION
+        SEGMENT: Flight SVQ 2023-03-02 06:40 -> BCN 09:10
+        \"\"\"
+      }
+  """
+  @spec read_file(file_path :: String.t())  :: {:ok, String.t()} | {:error, :enoent | :eacces | :eisdir | :enotdir | :enomem}
+  def read_file(file_path) when is_binary(file_path) do
+    case File.read(file_path) do
+      {:ok, binary} ->
+        Logger.debug("File #{inspect(file_path)} read successfuly.")
+        {:ok, binary}
+    end
+  end
+
+  @doc """
+  Decode the input content properly. Returns a 3 element tuple if okay:
+
+  * `:ok`
+  * `start_location` - the based field.
+  * `segments` list of segments read from the file.
+
+  ## Examples
+
+      iex> Reservator.Decoder.decode_content(
+      ...>   \"\"\"
+      ...>   BASED: SVQ
+      ...>
+      ...>   RESERVATION
+      ...>   SEGMENT: Flight SVQ 2023-03-02 06:40 -> BCN 09:10
+      ...>
+      ...>   RESERVATION
+      ...>   SEGMENT: Hotel BCN 2023-01-05 -> 2023-01-10
+      ...>   \"\"\"
+      ...> )
       {
         :ok,
         "SVQ",
@@ -25,66 +66,46 @@ defmodule Reservator.Decoder do
             end_location: "BCN"
           },
           %Reservator.Reservation.Segment{
-            segment_type: "Train",
-            start_time: ~N[2023-02-15 09:30:00],
-            start_location: "SVQ",
-            end_time: ~N[2023-02-15 11:00:00],
-            end_location: "MAD"
-          },
-          %Reservator.Reservation.Segment{
-            segment_type: "Flight",
-            start_time: ~N[2023-03-02 15:00:00],
+            segment_type: "Hotel",
+            start_time: ~N[2023-01-05 00:00:00],
             start_location: "BCN",
-            end_time: ~N[2023-03-02 22:45:00],
-            end_location: "NYC"
-          },
-          %Reservator.Reservation.Segment{
-            segment_type: "Flight",
-            start_time: ~N[2023-03-06 08:00:00],
-            start_location: "NYC",
-            end_time: ~N[2023-03-06 09:25:00],
-            end_location: "BOS"
+            end_time: ~N[2023-01-10 00:00:00],
+            end_location: "BCN"
           }
         ]
       }
   """
-  @spec decode_file(binary()) ::
-          {:ok, binary(), list(Segment.t())}
-          | {:error,
-             :deserialization_failed | :file_not_found | :file_read_error | :no_start_location}
-  def decode_file(file_path) when is_binary(file_path) do
-    with {:ok, binary} <- read_file(file_path),
-         {:ok, start_location} <- start_location(binary),
-         {:ok, segments} <- Segment.deserialize_segments(binary) do
+  @spec decode_content(String.t()) ::
+          {:ok, String.t(), list(Segment.t())}
+          | {:error, :deserialization_failed | :no_start_location}
+  def decode_content(file_content) when is_binary(file_content) do
+    with {:ok, start_location} <- start_location(file_content),
+         {:ok, segments} <- Segment.deserialize_segments(file_content) do
       Logger.debug("Deserialized #{length(segments)} segments")
       {:ok, start_location, segments}
     end
   end
 
+  @doc """
+  Find the first instance of `BASED: ([A-Z]{3})`.
+
+  ## Examples
+
+      iex> Reservator.Decoder.start_location("BASED: SVQ")
+      {:ok, "SVQ"}
+      
+      iex> Reservator.Decoder.start_location("BASED: SVQA")
+      {:error, :no_start_location}
+      
+  """
   @spec start_location(content :: String.t()) :: {:ok, String.t()} | {:error, :no_start_location}
-  defp start_location(content) when is_binary(content) do
-    case Regex.run(~r/BASED: (\w{3})$/m, content) do
-      [_match, start_location] ->
+  def start_location(content) when is_binary(content) do
+    case Regex.named_captures(@location_regex, content) do
+      %{"base" => start_location} ->
         {:ok, start_location}
 
       _ ->
         {:error, :no_start_location}
-    end
-  end
-
-  @spec read_file(file_path :: String.t()) ::
-          {:ok, String.t()} | {:error, :file_not_found} | {:error, :file_read_error}
-  defp read_file(file_path) do
-    with {:file_check, true} <- {:file_check, File.exists?(file_path)},
-         {:ok, binary} <- File.read(file_path) do
-      Logger.debug("Binary read file #{inspect(file_path)} successfuly.")
-      {:ok, binary}
-    else
-      {:file_check, false} ->
-        {:error, :file_not_found}
-
-      _ ->
-        {:error, :file_read_error}
     end
   end
 end
